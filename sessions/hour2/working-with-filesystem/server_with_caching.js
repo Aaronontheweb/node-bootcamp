e34a6f286069f86ecb522385255267435930824b
Matt <matt.pardee@gmail.com>
2012-02-15 03:53:57 +0000

    "Working with filesystem"

---
+++ sessions/hour2/working-with-filesystem/server_with_caching.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+
+var loadedFiles = {};
+
+function returnData(res, code, data) {
+    res.writeHead(code, {'Content-Type': 'text/plain'});
+    res.end(data);
+}
+
+http.createServer(function (req, res) {
+    if (loadedFiles[req.url])
+        returnData(res, 200, "Cached: " + loadedFiles[req.url]);
+
+    fs.readFile(__dirname + req.url, 'utf-8', function(err, data) {
+        if (err) {
+            returnData(res, 404, 'File ' + req.url + ' not found\n');
+        }
+        else {
+            loadedFiles[req.url] = data;
+            returnData(res, 200, data);
+        }
+    });
+}).listen(process.env.PORT || 80, "0.0.0.0");
