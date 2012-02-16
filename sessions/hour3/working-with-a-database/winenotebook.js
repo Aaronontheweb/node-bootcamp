da7b40a6c6b39507bab3036d38cbfd3da26d0cfc
Matt Harrington <Matt.Harrington@microsoft.com>
2012-02-16 09:40:37 -0800

    Added deleteEntity to winenotebook.js. Updated documentation.

--- sessions/hour3/working-with-a-database/winenotebook.js
+++ sessions/hour3/working-with-a-database/winenotebook.js
@@ -1,18 +1,18 @@
 var http = require('http');
 var formidable = require('formidable'), util = require('util');
 var uuid = require('node-uuid');
-var port = process.env.PORT || process.env.port || 1337;  // for C9, Azure, or local.
-var server = '127.0.0.1';
+var port = process.env.PORT || 1337;  // for C9, Azure, or when running locally.
 var azure = require('azure');
-var tableService = azure.createTableService(azure.ServiceClient.DEVSTORE_STORAGE_ACCOUNT, azure.ServiceClient.DEVSTORE_STORAGE_ACCESS_KEY, azure.ServiceClient.DEVSTORE_TABLE_HOST);
+var tableService = azure.createTableService(azure.ServiceClient.DEVSTORE_STORAGE_ACCOUNT,
+                azure.ServiceClient.DEVSTORE_STORAGE_ACCESS_KEY, azure.ServiceClient.DEVSTORE_TABLE_HOST);
 
-var tableName = 'wines2335'; // Name your table here.
+var tableName = 'wines'; // Name your table here.
 
 // Create the table
 tableService.createTableIfNotExists(tableName, tableCreatedOrExists);
 function tableCreatedOrExists(error) {
     if (error === null) {
-        console.log('Using table ' + tableName + '\r\n');
+        console.log('Using table ' + tableName);
     } else {
         console.log('Could not use table: ' + error.code);
     }
@@ -20,44 +20,64 @@ function tableCreatedOrExists(error) {
 
 function entityInsertedCallback(error, serverEntity) {
     if (error === null) {
-        console.log('Successfully inserted entity ' + serverEntity.Winery
-                    + ' \r\n');
+        console.log('Successfully inserted entity ' + serverEntity.Winery);
     } else {
         console.log('Could not insert entity into table: ' + error.code);
     }
 }
 
-http.createServer(function (req, res) {
+function entityDeletedCallback(error) {
+    if (error === null) {
+        console.log('Successfully deleted entity');
+    }
+    else {
+        console.log('Could not delete entity: ' + error.code);
+    }
+}
 
+http.createServer(function (req, res) {
     if (req.url === '/favicon.ico') return;
 
     if (req.url == '/addWine' && req.method.toLowerCase() == 'post') {
-        // parse a file upload
+        // Read http://nodebeginner.org to learn more about formidable.
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
             res.writeHead(200, { 'content-type': 'text/plain' });
-            res.write('received upload:\n\n');
-            res.end(util.inspect({ fields: fields, files: files }));
+            res.write('Received this via HTTP POST:\n\n');
+            res.write(util.inspect({ fields: fields }) + '\n\n');
+            res.end('Check console for status of adding this, and reload "/" to see it.');
             var wine = fields;
             wine['RowKey'] = uuid();
             wine['TastedOn'] = new Date();
-            wine[fields.AdditionalKey1] = fields.AdditionalValue1;
+            // Add additional keys and values to the entity, making sure they're not empty.  Then delete properties which
+            // were parsed from the incoming form data.
+            if (fields.AdditionalKey1 !== '') wine[fields.AdditionalKey1] = fields.AdditionalValue1;
+            if (fields.AdditionalKey2 !== '') wine[fields.AdditionalKey2] = fields.AdditionalValue2;
+            if (fields.AdditionalKey3 !== '') wine[fields.AdditionalKey3] = fields.AdditionalValue3;
             delete wine.AdditionalKey1; delete wine.AdditionalValue1;
-            wine[fields.AdditionalKey2] = fields.AdditionalValue2;
             delete wine.AdditionalKey2; delete wine.AdditionalValue2;
-            wine[fields.AdditionalKey3] = fields.AdditionalValue3;
             delete wine.AdditionalKey3; delete wine.AdditionalValue3;
             tableService.insertEntity(tableName, wine, entityInsertedCallback);
         });
-        //        res.writeHead(302, { 'Location': 'http://' + server + ':' + port,
-        //            'Cache-Control': 'no-cache' });
-        //        res.end();
         return;
     }
 
-    res.writeHead(200, { 'Content-Type': 'text/html', 'Cache-Control': 'no-cache' });
-    res.write('<h1>Wine Notebook</h1>');
+    if (req.url == '/deleteWine' && req.method.toLowerCase() == 'post') {
+        // delete a wine
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            res.writeHead(200, { 'content-type': 'text/plain' });
+            res.write('Received this via HTTP POST:\n\n');
+            res.write(util.inspect({ fields: fields }) + '\n\n');
+            res.end('Check console for status of deleting this, and reload "/" to see it.');
+            var wine = fields;
+            tableService.deleteEntity(tableName, wine, entityDeletedCallback);
+        });
+        return;
+    }
 
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Wine Notebook</h1>');
     var form = '<form action="/addWine" method="post">' +
             '<label for="Category">Category: </label><input name="PartitionKey" type="text" id="Category">' +
             '<label for="Winery">Winery: </label><input name="Winery" type="text" id="Winery"><br />' +
@@ -72,10 +92,9 @@ http.createServer(function (req, res) {
             '<label for="Notes">Notes: </label><textarea name="Notes" rows="6" cols="30" id="Notes"></textarea>' +
             '<input type="submit" value="Add" />' +
             '</form>'
-    res.write(form);
+    res.write(form + '<hr />');
 
-    // read data from table
-    console.log('reading data from table');
+    // read & display data from table
     var query = azure.TableQuery.select().from(tableName);
     tableService.queryEntities(query, entitiesQueriedCallback);
     function entitiesQueriedCallback(error, serverEntities) {
@@ -83,11 +102,15 @@ http.createServer(function (req, res) {
             res.write('<ul>');
             for (var index in serverEntities) {
                 res.write('<li>' + serverEntities[index].Winery);
-                //res.write('<input type="button" name="' + serverEntities[index].RowKey + '" value="Delete" />');
+                res.write('<form action="/deleteWine" method="post">');
+                res.write('<input type="hidden" name="PartitionKey" value="' + serverEntities[index].PartitionKey + '" />');
+                res.write('<input type="hidden" name="RowKey" value="' + serverEntities[index].RowKey + '" />');
+                res.write('<input type="submit" value="Delete" />');
+                res.write('</form>');
                 res.write('<ul>');
                 for (var prop in serverEntities[index]) {
-                    if (prop === 'id' || prop === 'RowKey' || prop === 'Timestamp' || prop === 'etag'
-                            || prop === 'link' || prop === 'updated') continue;
+                    if (prop === 'id' || prop === 'Timestamp' || prop === 'etag' ||
+                            prop === 'RowKey' || prop === 'link' || prop === 'updated') continue;
                     if (prop === 'PartitionKey') { // PartitionKey is 'red', 'white', 'rose', etc.
                         res.write('<li>Category: ' + serverEntities[index][prop]);
                         continue;
@@ -103,5 +126,4 @@ http.createServer(function (req, res) {
         }
     }
 
-}).listen(port, server);
-console.log('Server running at http://' + server + ':' + port + '/');
+}).listen(port);
