3fc60238f485eb3863cbbaa6caac2621e4fe1f62
Matt Harrington <Matt.Harrington@microsoft.com>
2012-02-17 11:07:17 -0800

    Renamed sample.

--- sessions/hour3/using-continuation-tokens-for-pagination/table-storage-pagination-sample.js
+++ sessions/hour3/using-continuation-tokens-for-pagination/table-storage-pagination-sample.js
@@ -17,10 +17,9 @@ if (process.env.C9_PORT) { // Test if we're running on Cloud9. Change these to y
 
 var tableService = azure.createTableService(account, accountKey, tableHost);
 //tableService.logger = new azure.Logger(azure.Logger.LogLevels.DEBUG);  // Uncomment this to enable logging
-var totalEntities = 100; // number of entities to create
+var totalEntities = 100; // Number of entities to create. Make it a multiple of 100.
 var pageSize = 20;
 var tableName = 'largetable2231'; // Name your table here.
-var query = azure.TableQuery.select().from(tableName).top(pageSize);
 
 // Create and populate the table.  Note that for batch operations,
 // the PartitionKey must be the same for all entities.
@@ -31,7 +30,9 @@ tableService.createTableIfNotExists(tableName, function (err, created) {
             tableService.beginBatch();
             var now = new Date().toString();
             for (var j = 0; j < 100; j++) { // Batches can include at most 100 entities.
-                tableService.insertEntity(tableName, { PartitionKey: 'White', RowKey: (i * 100 + j + 1).toString(), Winery: 'Azure Vineyards', Variety: 'Chardonnay', Vintage: '2003', Notes: uuid(), TastedOn: now });
+                tableService.insertEntity(tableName, { PartitionKey: 'White', RowKey: (i * 100 + j + 1).toString(),
+                    Winery: 'Azure Vineyards', Variety: 'Chardonnay', Vintage: '2003', Notes: uuid(), TastedOn: now
+                });
             }
             tableService.commitBatch(function () {
                 console.log('Initialized table "' + tableName + '" with 100 sample entities.');
@@ -41,6 +42,7 @@ tableService.createTableIfNotExists(tableName, function (err, created) {
 });
 
 http.createServer(function (req, res) {
+    var counter = 0;
     if (req.url === '/favicon.ico') return;
 
     if (url.parse(req.url).pathname === '/nextPage') {
@@ -51,20 +53,22 @@ http.createServer(function (req, res) {
         return;
     }
 
+    var query = azure.TableQuery.select().from(tableName).top(pageSize);
     tableService.queryEntities(query, entitiesQueriedCallback);
-    var counter = 0;
 
     function entitiesQueriedCallback(error, serverEntities, pageContinuation) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         if (error === null) {
             for (var index in serverEntities) {
-                res.write(serverEntities[index].RowKey + '<br />');
+                res.write('RowKey: ' + serverEntities[index].RowKey + ', Winery: ' +
+                            serverEntities[index].Winery + ', Notes: ' + serverEntities[index].Notes + '<br />');
                 counter++;
             }
-            res.write('Found ' + counter + ' entities. <br />');
-
-            res.write('<a href="/nextPage?nextPartitionKey=' + pageContinuation.nextPartitionKey +
-                '&nextRowKey=' + pageContinuation.nextRowKey + '">Next page</a>');
+            res.write('<br />Returned ' + counter + ' entities. <br />');
+            if (pageContinuation.hasNextPage()) {
+                res.write('<a href="/nextPage?nextPartitionKey=' + pageContinuation.nextPartitionKey +
+                    '&nextRowKey=' + pageContinuation.nextRowKey + '">Next page</a>');
+            }
             res.end();
         } else {
             res.end('Could not query entities: ' + error.code);
@@ -73,8 +77,8 @@ http.createServer(function (req, res) {
     }
 
     function listNextPage(partitionKey, rowKey) {
-        var query = azure.TableQuery.select().from(tableName).top(pageSize).whereNextKeys(partitionKey, rowKey);
-        tableService.queryEntities(query, entitiesQueriedCallback);
+        var nextPageQuery = azure.TableQuery.select().from(tableName).top(pageSize).whereNextKeys(partitionKey, rowKey);
+        tableService.queryEntities(nextPageQuery, entitiesQueriedCallback);
     }
 
 }).listen(port);
