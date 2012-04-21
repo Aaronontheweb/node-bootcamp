a7147d885fda312772b17565e50226a1fb3bd9a4
Aaron Stannard <aaron@aaronstannard.com>
2012-04-21 00:24:38 +0000

    added the python and node examples of reading from the filesystem

---
+++ sessions/hour0/readFile.py
@@ -0,0 +1,9 @@
+""" PYTHON """
+import os
+
+filePath = os.path.join(os.getcwd(), "README.md")
+
+if not (os.path.exists(filePath)):
+    print "Could not read file %s" % filePath
+else:
+    print open(filePath, "r").readLines()
